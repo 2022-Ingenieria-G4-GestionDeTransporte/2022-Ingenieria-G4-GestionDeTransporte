d95974a78777b6840dad06643205715559917789
xrbarrantes11 <93897708+xrbarrantes11@users.noreply.github.com>
2022-10-05 19:00:49 -0600

    Update listaGiras.php

--- View/viewFuncionario/listaGiras.php
+++ View/viewFuncionario/listaGiras.php
@@ -3,7 +3,7 @@
 
 <html xml:th="http://thymeleaf.org">
 <head>
-    <title>Vehiculos</title>
+    <title>Giras</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
