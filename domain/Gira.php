7da0cdad19b5a1e845648e086808f535aa8bf2ab
eversolano <ever10solano@hotmail.com>
2022-10-14 14:12:58 -0600

    update gira.php file

--- domain/Gira.php
+++ domain/Gira.php
@@ -33,4 +33,129 @@ class Gira{
         $this->giraObjetivo = $giraObjetivo;
         $this->giraIdChofer = $giraIdChofer;
     }
+	
+	function getGiraId() {
+		return $this->giraId;
+	}
+	
+	function setGiraId($giraId){
+		$this->giraId = $giraId;
+	}
+	
+	function getGiraNombreEncargado() {
+		return $this->giraNombreEncargado;
+	}
+	
+	function setGiraNombreEncargado($giraNombreEncargado) {
+		$this->giraNombreEncargado = $giraNombreEncargado;
+
+	}
+
+	function getGiraApellidoEncargado() {
+		return $this->giraApellidoEncargado;
+	}
+	
+	function setGiraApellidoEncargado($giraApellidoEncargado) {
+		$this->giraApellidoEncargado = $giraApellidoEncargado;
+
+	}
+
+	function getGiraCedulaEncargado() {
+		return $this->giraCedulaEncargado;
+	}
+
+	function setGiraCedulaEncargado($giraCedulaEncargado) {
+		$this->giraCedulaEncargado = $giraCedulaEncargado;
+	}
+	
+	function getGiraFechaSolicitud() {
+		return $this->giraFechaSolicitud;
+	}
+	
+	function setGiraFechaSolicitud($giraFechaSolicitud) {
+		$this->giraFechaSolicitud = $giraFechaSolicitud;
+
+	}
+
+	function getGiraNombreAcompanante() {
+		return $this->giraNombreAcompanante;
+	}
+	
+	
+	function setGiraNombreAcompanante($giraNombreAcompanante) {
+		$this->giraNombreAcompanante = $giraNombreAcompanante;
+	
+	}
+
+	function getGiraApellidoACompanante() {
+		return $this->giraApellidoACompanante;
+	}
+
+	function setGiraApellidoACompanante($giraApellidoACompanante) {
+		$this->giraApellidoACompanante = $giraApellidoACompanante;
+
+	}
+	
+	function getGiraCedulaAcompanante() {
+		return $this->giraCedulaAcompanante;
+	}
+
+	function setGiraCedulaAcompanante($giraCedulaAcompanante) {
+		$this->giraCedulaAcompanante = $giraCedulaAcompanante;
+
+	}
+	
+	function getGiraFechaGira() {
+		return $this->giraFechaGira;
+	}
+	
+	function setGiraFechaGira($giraFechaGira) {
+		$this->giraFechaGira = $giraFechaGira;
+
+	}
+	
+	function getGiraCarrera() {
+		return $this->giraCarrera;
+	}
+	
+	function setGiraCarrera($giraCarrera) {
+		$this->giraCarrera = $giraCarrera;
+	
+	}
+	
+	function getGiraTipoGira() {
+		return $this->giraTipoGira;
+	}
+
+	function setGiraTipoGira($giraTipoGira) {
+		$this->giraTipoGira = $giraTipoGira;
+		
+	}
+
+	function getGiraRequireChofer() {
+		return $this->giraRequireChofer;
+	}
+	
+	function setGiraRequireChofer($giraRequireChofer) {
+		$this->giraRequireChofer = $giraRequireChofer;
+
+	}
+	
+	function getGiraObjetivo() {
+		return $this->giraObjetivo;
+	}
+	
+	function setGiraObjetivo($giraObjetivo) {
+		$this->giraObjetivo = $giraObjetivo;
+		
+	}
+	
+	function getGiraIdChofer() {
+		return $this->giraIdChofer;
+	}
+	
+	function setGiraIdChofer($giraIdChofer) {
+		$this->giraIdChofer = $giraIdChofer;
+	
+	}
 }
