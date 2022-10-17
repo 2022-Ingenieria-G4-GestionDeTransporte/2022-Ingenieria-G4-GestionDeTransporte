4d53a5585266e9d45bb30bf7cff0a9d372a72eb1
eversolano <ever10solano@hotmail.com>
2022-10-16 22:20:44 -0600

    mod solictantes

--- data/SolicitantesData.php
+++ data/SolicitantesData.php
@@ -9,14 +9,14 @@ class SolcitantesData extends Data{
         $querySelect = "SELECT * FROM tbsolicitante;";
         $result = mysqli_query($conn, $querySelect);
         mysqli_close($conn);
-        $Giras = [];
+        $Solicitantes = [];
         while ($row = mysqli_fetch_array($result)) {
             $currentSolicitante = new Solicitante($row['tbsolicitanteid'], $row['tbsolicitantecedula'], $row['tbsolicitantenombre'], 
             $row['tbsolicitanteapellidos'], $row['tbsolicitantecorreo'], $row['tbsolicitantecargo'], $row['tbsolicitantelicenciavigente']
             , $row['tbsolicitantefechavencimientolicencia'], $row['tbsolicitantepassword']);
-            array_push($Giras, $currentSolicitante);
+            array_push($Solicitantes, $currentSolicitante);
         }
-        return $Giras;
+        return $Solicitantes;
     }
 }
 
