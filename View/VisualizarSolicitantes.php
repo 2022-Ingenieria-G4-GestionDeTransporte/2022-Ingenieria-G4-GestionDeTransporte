32cd99ba94dd5c71d776a9f04a221bc0d6732d99
Derrek <derrek.urena.solis@est.una.ac.cr>
2022-11-04 18:41:03 -0600

    changes css

--- View/VisualizarSolicitantes.php
+++ View/VisualizarSolicitantes.php
@@ -8,7 +8,7 @@
     include '../Business/SolicitanteBusiness.php';
     ?>
     </link>
-    <link rel = "stylesheet" href="../css/style.css" type = "text/css"></link>
+    <link rel = "stylesheet" href="../css/style1.css" type = "text/css"></link>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     <link rel="stylesheet" href="myProjects/webProject/icofont/css/icofont.min.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
