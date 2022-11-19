98ab29e3d301f3c853e7fcbf700c359ce25039da
Derrek <derrek.urena.solis@est.una.ac.cr>
2022-11-18 18:29:41 -0600

    changes

--- View/Login.php
+++ View/Login.php
@@ -12,15 +12,15 @@ error_reporting(0);
     include '../business/LoginBusiness.php';
     ?>
 </head>
-<body style="background-color: #AA041B;"  class="body">
-<section class="vh-100" >
+<body class="body">
+<section class="vh-100" style="background-color: #AA041B;">
   <div class="container py-5 h-50">
     <div class="row d-flex justify-content-center align-items-center h-100">
       <div class="col col-xl-10">
         <div class="card" style="border-radius: 1rem;">
           <div class="row g-0">
             <div class="col-md-6 col-lg-5 d-none d-md-block">
-              <img src="../Images/registros.png" 
+              <img src="../Images/Login.jpg" 
                 alt="login form" class="img-fluid" style="border-radius: 1rem 0 0 1rem;" />
             </div>
             <div class="col-md-6 col-lg-7 d-flex align-items-center">
@@ -28,39 +28,35 @@ error_reporting(0);
 
                 <form method="post" enctype="multipart/form-data" action="../Business/LoginAction.php">
 
+                  <div class="d-flex align-items-center mb-3 pb-1">
                   <div class="d-flex align-items-center mb-1 pb-1">
                     <i class="fas fa-cubes fa-2x me-3" style="color: #ff6219;"></i>
-                    
-                    <span align="center" class="h1 fw-bold mb-0">  Universidad Nacional De Costa Rica</span>
+                    <img src="../Images/Logo-UNA.png" width="70" height="60">
+                    <span class="h1 fw-bold mb-0">  Universidad Nacional</span>
                   </div>
                   
                   <center><h5 class="h5 fw-bold mb-0">Sistema Gestión de Transporte</h5></center></br>
-                  
-                  <h5 align="center"  class="fw-normal mb-3 pb-2" style="letter-spacing: 1px;">Ingrese sus datos</h5>
+                  <h5 class="fw-normal mb-3 pb-2" style="letter-spacing: 1px;">Ingrese sus datos</h5>
 
-                  <div align="center"  class="form-outline mb-4">
-                  <label  class="form-label" for="cedula">Cédula:</label>
-                    <input type="text" name = "cedula" id="cedula" required placeholder = "                        Ingrese su cédula" class="form-control form-control-lg" />
-                    
+                  <div class="form-outline mb-4">
+                    <input type="text" name = "cedula" id="cedula" required placeholder = "Ingrese su cedula" class="form-control form-control-lg" />
+                    <label class="form-label" for="cedula">Cedula</label>
                   </div>
 
-                  <div align="center"  class="form-outline mb-4">
-                  <label class="form-label" for="password">Contraseña:</label>
-                    <input type="password" name = "password" id="password" required placeholder = "                   Ingrese su contraseña " class="form-control form-control-lg" />
-                    
+                  <div class="form-outline mb-4">
+                    <input type="password" name = "password" id="password" required placeholder = "Ingrese su contraseña " class="form-control form-control-lg" />
+                    <label class="form-label" for="password">Contraseña</label>
                   </div>
 
-                  <div align="center" class="pt-0.1 mb-4">
+                  <div class="pt-0.1 mb-4">
                     <button class="btn btn-dark btn-lg btn-block" name="login" id="login" type="submit">Ingresar</button>
                   </div>
 
-                  <div align="center">
                   <a class="small text-muted" href="#!">¿Olvidaste tu contraseña?</a>
-                  <p class="mb-5 pb-lg-2" style="color: #393f81;">¿No tienes una cuenta? 
-                  <a href="/2022-Ingenieria-G4-GestionDeTransporte/View/RegistroSolicitanteInicio.php" style="color: #393f81;">Regístrese aquí</a></p>
+                  <p class="mb-5 pb-lg-2" style="color: #393f81;">¿No tienes una cuenta? <a href="#!"
+                      style="color: #393f81;">Regístrese aquí</a></p>
                   <a href="#!" class="small text-muted">Terms of use.</a>
                   <a href="#!" class="small text-muted">Privacy policy</a>
-</div>
                 </form>
 
               </div>
