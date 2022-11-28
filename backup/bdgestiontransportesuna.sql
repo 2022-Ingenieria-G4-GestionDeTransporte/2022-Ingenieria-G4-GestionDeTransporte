502c7b8c5338b5a1141854c26507d2bebedd0af8
eversolano <ever10solano@hotmail.com>
2022-11-28 02:07:46 -0600

    new bd

--- backup/bdgestiontransportesuna.sql
+++ backup/bdgestiontransportesuna.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 28-11-2022 a las 08:58:46
+-- Tiempo de generación: 28-11-2022 a las 09:07:28
 -- Versión del servidor: 10.4.25-MariaDB
 -- Versión de PHP: 7.4.30
 
